c0e0d70801330c6cd86025282c72a3af5671876f
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-31 00:45:39 -0600

    Agregar archivos de proyecto.

---
+++ Nombres_edad_mayor_de_edad/Program.cs
@@ -0,0 +1,29 @@
+﻿string nombre1;
+string nombre2;
+int edad1;
+int edad2;
+string num;
+
+Console.WriteLine("Ingrese el Nombre: ");
+nombre1 = Console.ReadLine();
+Console.Write("Ingrese  la Edad ==>");
+num = Console.ReadLine();
+edad1 = int.Parse(num);
+
+Console.WriteLine("Ingrese el Nombre: ");
+nombre2 = Console.ReadLine();
+Console.Write("Ingrese  la Edad ==>");
+num = Console.ReadLine();
+edad2 = int.Parse(num);
+
+Console.WriteLine("La Persona Mayor de Edad es: ");
+
+if (edad1 > edad2)
+{
+    Console.Write(nombre1);
+}
+else
+{
+    Console.Write(nombre2);
+}
+Console.ReadKey(true);
